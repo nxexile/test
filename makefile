57658c3ae820f26403c36a264b61425ed3179234
nxexile <70866762+nxexile@users.noreply.github.com>
2020-11-02 13:19:14 -0500

    Add files via upload

---
+++ makefile
@@ -0,0 +1,3 @@
+all:
+	gcc -pthread -o votecountersem votecountersem.c
+	gcc -pthread -o pizzastudy pizzastudy.c
---
+++ pizzastudy.c
@@ -0,0 +1,68 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <sys/syscall.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+int pizza = 1;
+int slice = 1;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *eating (void *);
+void *deliver (void *);
+
+int main(int argc, char *argv[]){
+	int students = atoi(argv[1]);
+	int count=0;
+	pthread_t sthreads[students];
+	pthread_t delivery;
+	printf("Number of students: %d\n",students);
+	//scanf("%d",&n);
+	//thread = (pthread_t *) malloc (students*sizeof(pthread_t));
+	while(pizza < 13){
+	//pthread_mutex_lock(&mutex);
+	if(slice == 8){
+		pthread_create(&delivery,NULL,deliver,NULL);
+	}
+	else if( students > 1 && students < 6 ){
+	//pthread_mutex_lock(&mutex);
+	for(count=0; count<students && slice<8;count++){
+		printf("I am Student %d\n",count+1);
+		pthread_create(&sthreads[count],NULL,eating,(void *)(intptr_t)count);
+		}
+	//pthread_mutex_unlock(&mutex);	
+	}
+	else{
+	printf("Error please enter a number between 1-10 next time.\n");
+	exit(0);
+	}
+	//pthread_mutex_unlock(&mutex);
+	}
+	if( students > 1 && students < 6){
+	for(count=0; count<students;count++){
+		pthread_join(sthreads[count],NULL);
+	}
+	}
+	pthread_join(delivery,NULL);
+	return(0);	
+}
+
+void *eating (void *arg){
+    int studentid = (int)(intptr_t)arg;
+    printf("Student %d is eating a slice of pizza %d\n",studentid+1,pizza);
+	slice++;
+	sleep(rand() % 3);
+	
+}
+
+void *deliver (void *arg){
+    printf("pizza %d is delivered \n", pizza);
+	slice = 1;
+	pizza++;
+	sleep(rand() % 3);
+
+}
